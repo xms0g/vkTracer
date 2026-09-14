94a9f109d685d55d46ffcce70633d22e5619252f
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2026-09-14 04:56:31 +0300

    feat(shader): implement basic ray tracing with sphere intersection and background rendering

--- src/config/config.hpp
+++ src/config/config.hpp
@@ -5,8 +5,9 @@
 #define SHADER_BINARY_DIR "shaders/"
 #endif
 
+constexpr float aspect = 16.0f / 9.0f;
 constexpr uint32_t WIDTH{800};
-constexpr uint32_t HEIGHT{600};
+constexpr uint32_t HEIGHT{static_cast<uint32_t>(WIDTH / aspect)};
 constexpr int32_t MAX_FRAMES_IN_FLIGHT{2};
 constexpr uint32_t THREADS_PER_GROUP{8};
 constexpr auto SHADER_NAME = "main.spv";
