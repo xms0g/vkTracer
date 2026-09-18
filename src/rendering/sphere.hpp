3189dca1760822eaf8dfa5561be8929f7337cd3d
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2026-09-18 19:13:47 +0300

    refactor(rendering & shader): introduce `Material` struct and unify material handling
    - Replaced `MaterialType` and albedo fields with a unified `Material` struct in `Sphere` and `HitRecord`.
    - Updated sphere generation and hit detection logic to use the new `Material` struct.
    - Simplified shader logic by refactoring material-specific scattering and removing redundant fields.

--- src/rendering/sphere.hpp
+++ src/rendering/sphere.hpp
@@ -6,37 +6,49 @@ enum class MaterialType: uint32_t {
 	Metal = 1
 };
 
+struct Material {
+	glm::vec3 albedo;
+	MaterialType type;
+};
+
 struct Sphere {
 	glm::vec3 center;
 	float radius;
-	glm::vec3 albedo;
-	MaterialType mat;
+	Material mat;
 
 	static std::vector<Sphere> generateSpheres() {
 		std::vector<Sphere> spheres = {
 			{
 				.center = {-1.0, 0.0, -1.0f},
 				.radius = 0.5f,
-				.albedo = {0.8, 0.8, 0.8},
-				.mat = MaterialType::Metal
+				.mat = {
+					.albedo = {0.8, 0.8, 0.8},
+					.type = MaterialType::Metal
+				}
 			},
 			{
 				.center = {1.0, 0.0, -1.0},
 				.radius = 0.5f,
-				.albedo = {0.8, 0.6, 0.2},
-				.mat = MaterialType::Metal
+				.mat = {
+					.albedo = {0.8, 0.6, 0.2},
+					.type = MaterialType::Metal
+				}
 			},
 			{
 				.center = {0.0f, 0.0f, -1.0f},
 				.radius = 0.5f,
-				.albedo = {0.1, 0.2, 0.5},
-				.mat = MaterialType::Lambertian
+				.mat = {
+					.albedo = {0.1, 0.2, 0.5},
+					.type = MaterialType::Lambertian
+				}
 			},
 			{
 				.center = {0.0f, -100.5f, -1.0f},
 				.radius = 100.0f,
-				.albedo = {0.8, 0.8, 0.0},
-				.mat = MaterialType::Lambertian
+				.mat = {
+					.albedo = {0.8, 0.8, 0.0},
+					.type = MaterialType::Lambertian
+				}
 			}
 		};
 
