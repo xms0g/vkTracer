ee6f83dcb7b4e4a9ac22cf791fb0f7c143a2a44c
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2026-09-18 04:55:47 +0300

    feat(shader & rendering): add material system with Lambertian and Metal support
    - Introduced `MaterialType` enum to distinguish between Lambertian and Metal materials.
    - Added scattering logic for Lambertian and Metal types in the new `material.slang` module.
    - Extended `HitRecord` and `Sphere` structs to store material properties and albedo.
    - Integrated material-based light scattering into `rayColor` for realistic shading.
    - Updated sphere generation in `sphere.hpp` and shader logic to support materials.

--- src/rendering/sphere.hpp
+++ src/rendering/sphere.hpp
@@ -1,14 +1,43 @@
 #pragma once
 #include <glm/glm.hpp>
 
+enum class MaterialType: uint32_t {
+	Lambertian = 0,
+	Metal = 1
+};
+
 struct Sphere {
 	glm::vec3 center;
 	float radius;
+	glm::vec3 albedo;
+	MaterialType mat;
 
 	static std::vector<Sphere> generateSpheres(const uint32_t count) {
 		std::vector<Sphere> spheres = {
-			{.center = { 0.0f,  0.0f, -1.0f }, .radius = 0.5f},
-			{.center = { 0.0f, -100.5f, -1.0f }, .radius = 100.0f}
+			{
+				.center = {-1.0, 0.0, -1.0f},
+				.radius = 0.5f,
+				.albedo = {0.8, 0.8, 0.8},
+				.mat = MaterialType::Metal
+			},
+			{
+				.center = {1.0, 0.0, -1.0},
+				.radius = 0.5f,
+				.albedo = {0.8, 0.6, 0.2},
+				.mat = MaterialType::Metal
+			},
+			{
+				.center = {0.0f, 0.0f, -1.0f},
+				.radius = 0.5f,
+				.albedo = {0.1, 0.2, 0.5},
+				.mat = MaterialType::Lambertian
+			},
+			{
+				.center = {0.0f, -100.5f, -1.0f},
+				.radius = 100.0f,
+				.albedo = {0.8, 0.8, 0.0},
+				.mat = MaterialType::Lambertian
+			}
 		};
 
 		return spheres;
