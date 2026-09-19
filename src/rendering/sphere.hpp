4c7fe282c954c08a5c0bd88f2fd7ed3d182f94a4
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2026-09-19 17:53:43 +0300

    feat(rendering): enhance sphere generation with randomized materials and dynamic placement
    - Added random sphere generation logic with varied materials (Lambertian, Metal, Dielectric).
    - Implemented dynamic placement and distribution across a configurable range.
    - Updated sphere definitions to improve rendering diversity and realism.

--- src/rendering/sphere.hpp
+++ src/rendering/sphere.hpp
@@ -22,36 +22,66 @@ struct Sphere {
 	static std::vector<Sphere> generateSpheres() {
 		std::vector<Sphere> spheres = {
 			{
-				.centerRadius = {-1.0, 0.0, -1.0f, 0.5f},
+				.centerRadius = {0.0, 1.0, 0.0f, 1.0f},
 				.mat = {
-					.albedo = {0.8, 0.8, 0.8, 0.0},
 					.type = MaterialType::Dielectric,
-					.refractionIndex = 1.0 / 1.33
+					.refractionIndex = 1.5
 				}
 			},
 			{
-				.centerRadius = {1.0, 0.0, -1.0, 0.5f},
+				.centerRadius = {-4.0, 1.0, 0.0, 1.0f},
 				.mat = {
-					.albedo = {0.8, 0.6, 0.2, 0.0},
+					.albedo = {0.4, 0.2, 0.1, 0.0},
+					.type = MaterialType::Lambertian,
+				}
+			},
+			{
+				.centerRadius = {4.0f, 1.0f, 0.0f, 1.0f},
+				.mat = {
+					.albedo = {0.7, 0.6, 0.5, 0.0},
 					.type = MaterialType::Metal,
-					.fuzz = 0.0f
 				}
 			},
 			{
-				.centerRadius = {0.0f, 0.0f, -1.0f, 0.5f},
+				.centerRadius = {0.0f, -100.5f, 0.0f, 100.5f},
 				.mat = {
-					.albedo = {0.1, 0.2, 0.5, 0.0},
+					.albedo = {0.5, 0.5, 0.5, 0.0},
 					.type = MaterialType::Lambertian
 				}
 			},
-			{
-				.centerRadius = {0.0f, -100.5f, -1.0f, 100.0f},
-				.mat = {
-					.albedo = {0.8, 0.8, 0.0, 0.0},
-					.type = MaterialType::Lambertian
+		};
+
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_real_distribution<float> dis;
+
+		for (int a = -5; a < 5; a++) {
+			for (int b = -5; b < 5; b++) {
+				const auto chooseMat = dis(gen);
+				glm::vec3 center(a + 0.9 * dis(gen), 0.2, b + 0.9 * dis(gen));
+				auto centerRad = glm::vec4(center, 0.2);
+
+				if (glm::length(center - glm::vec3(4, 0.2, 0)) > 0.9) {
+					if (chooseMat < 0.8) {
+						// diffuse
+						const auto albedo = glm::vec4(dis(gen), dis(gen), dis(gen), 0.0f);
+						Material mat = {.albedo = albedo, .type = MaterialType::Lambertian};
+						spheres.emplace_back(centerRad, mat);
+					} else if (chooseMat < 0.95) {
+						std::uniform_real_distribution<float> albedoDis(0.5, 1);
+						std::uniform_real_distribution<float> fuzzDis(0, 0.5);
+						// metal
+						const auto albedo = glm::vec4(albedoDis(gen), albedoDis(gen), albedoDis(gen), 0.0f);
+						Material mat = {.albedo = albedo, .type = MaterialType::Metal, .fuzz = fuzzDis(gen)};
+						spheres.emplace_back(centerRad, mat);
+					} else {
+						// glass
+						Material mat = {.type = MaterialType::Dielectric, .refractionIndex = 1.5f};
+						spheres.emplace_back(centerRad, mat);
+					}
 				}
 			}
-		};
+		}
 
 		return spheres;
 	}
